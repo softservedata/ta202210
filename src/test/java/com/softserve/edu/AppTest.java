cc1a1afa13deb3a149528784f7bef7ebdb34f49b
Jaroslav <garasym@gmail.com>
2022-10-25 09:49:10 +0300

    Maven. Template Project. Bug Fixed

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -9,7 +9,14 @@ import org.testng.annotations.Test;
 public class AppTest {
 
     @Test
-    public void testApp() {
+    public void testApp1() {
+        System.out.println("class AppTest: testApp1()");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testApp2() {
+        System.out.println("class AppTest: testApp2()");
         Assert.assertTrue(true);
     }
 }
