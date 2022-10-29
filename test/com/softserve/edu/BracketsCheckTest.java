69380451236c5784b8fb1a0a37c651902236c7e2
OleksiiLiapin <liapinoleksii@gmail.com>
2022-10-29 16:37:16 +0300

    parameterized tests added

--- test/com/softserve/edu/BracketsCheckTest.java
+++ test/com/softserve/edu/BracketsCheckTest.java
@@ -2,19 +2,23 @@ package com.softserve.edu;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 
 public class BracketsCheckTest {
-    @Test
-    public void testBrackets(){
+    @ParameterizedTest
+    @ValueSource (strings = {"())",")(","(()"})
+    public void testBracketsNegative(String strings){
         BracketsCheck br = new BracketsCheck();
-
-        Assert.assertEquals(false, br.verifyBrackets("())"));
-        Assert.assertEquals(false, br.verifyBrackets(")("));
-        Assert.assertEquals(false, br.verifyBrackets("(()"));
-        Assert.assertEquals(true, br.verifyBrackets("()"));
-        Assert.assertEquals(true, br.verifyBrackets("(())"));
-        Assert.assertEquals(true, br.verifyBrackets("(())()"));
-
-
+        Assert.assertEquals(false, br.verifyBrackets(strings));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"()","(())","(())()"})
+    public void testBrackets(String strings){
+        BracketsCheck br = new BracketsCheck();
+        Assert.assertEquals(true, br.verifyBrackets(strings));
+    }
+
 }
