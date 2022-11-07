5db1a2337d6326117bbf089f9298503ecc12f175
OleksiiLiapin <liapinoleksii@gmail.com>
2022-11-08 00:41:39 +0200

    replace assert methods

--- test/com/softserve/edu/BracketsCheckTest.java
+++ test/com/softserve/edu/BracketsCheckTest.java
@@ -21,7 +21,7 @@ public class BracketsCheckTest {
     public static Object [][] bracketSet(){
         return new Object[][] {
                 {"())", false}, {")(", false}, {"(()", false},
-                {"()", true}, {"(())", true}, {"(())()", true},{"(1(name))(a)", true}
+                {"()", true}, {"(())", true}, {"(())()", true}, {"(1(name))(a)", true}
         };
     }
 
@@ -33,26 +33,29 @@ public class BracketsCheckTest {
     }
 
 
-    @Test (testName = "hw2", description = "verify the brackets",
+    @Test (testName = "hw2", description = "verify the all the brackets close",
             dataProvider = "testbracketsvalid" )
     public void testbracketsvalid(String strings, boolean value){
-        Assert.assertEquals(value, br.verifyBracketsEquity(strings));
+        Assert.assertEquals(br.verifyBracketsEquity(strings), value);
+    }
+
+    @Test
+    public void testbracketsvalid11(){
+        Assert.assertEquals(br.verifyBracketsEquity(")("), false);
     }
 
     @Test(description = "count the number of brackets",
     dataProvider = "testBracketsNum")
     public void numberBracketsTest(String strings, int num){
-        Assert.assertEquals(num, br.numberBrackets(strings));
+        Assert.assertEquals(br.numberBrackets(strings), num);
     }
 
     @Test (testName = "hw#3",
-            description = "count the number of brackets with the mock equty method",
-    dataProvider = "testBracketsNum")
+            description = "count the number of brackets with the spy equity method",
+    dataProvider = "testBracketsNum" )
     public void numberBracketsTest2(String strings, int num){
         br = spy(BracketsCheck.class);
         when(br.verifyBracketsEquity(anyString())).thenReturn(true);
-        Assert.assertEquals(num, br.numberBrackets(strings));
+        Assert.assertEquals(br.numberBrackets(strings), num);
     }
-
-
 }
