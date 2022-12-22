acbe3537d4f6fe25336d8e654c92cdca60c28ded
bebebe <hahaha@gmail.com>
2022-12-22 10:30:45 +0200

    Slf4j+Log4j

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -1,13 +1,19 @@
 package com.softserve.edu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        Thread.sleep(1);
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        System.out.println("Text Text Text Text Text Text Text Text Text Text Text ");
+        throw new RuntimeException("Error Error Error Error Error Error Error");
     }
 }
